d01061bdc0e0f3eab05d5c11ff8c2b1ccfa45927
jfourmond <jerome@fourmond.fr>
2016-04-05 18:35:19 +0200

    Modification sur la représentation d'un arc
    * Edge doit être déclaré sur le type d'une implémentation de Vertex

--- Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Edge.java
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Edge.java
@@ -2,53 +2,55 @@ package fr.fourmond.jerome.framework;
 
 /**
  * {@link Edge} représente un arc
- * @param <T>	Valeur de retour de l'arc 
+ * @param <T> Valeur de retour de l'arc
+ * @param <Vertex_T> Type des sommets, sous contraintes qu'il etende {@link Vertex}
+ * @param <Value_T> Type du libellé de l'arc 
  * @author jfourmond
  */
-public class Edge<T> {
-	private Vertex firstVertex;
-	private Vertex secondVertex;
-	private T value;
+public class Edge<Vertex_T extends Vertex, Value_T> {
+	private Vertex_T firstVertex;
+	private Vertex_T secondVertex;
+	private Value_T value;
 	
 	
 	//	CONSTRUCTEURS
 	public Edge() {};
 	
-	public Edge(Vertex firstVertex, Vertex secondVertex) {
+	public Edge(Vertex_T firstVertex, Vertex_T secondVertex) {
 		this.firstVertex = firstVertex;
 		this.secondVertex = secondVertex;
 	}
 	
-	public Edge(Vertex firstVertex, Vertex secondVertex, T value) {
+	public Edge(Vertex_T firstVertex, Vertex_T secondVertex, Value_T value) {
 		this.firstVertex= firstVertex;
 		this.secondVertex = secondVertex;
 		this.value = value;
 	}
 
 	//	GETTERS
-	public Vertex getFirstVertex() {
+	public Vertex_T getFirstVertex() {
 		return firstVertex;
 	}
 
-	public Vertex getSecondVertex() {
+	public Vertex_T getSecondVertex() {
 		return secondVertex;
 	}
 
-	public T getValue() {
+	public Value_T getValue() {
 		return value;
 	}
 
 	//	SETTERS
-	public void setFirstVertex(Vertex firstVertex) {
+	public void setFirstVertex(Vertex_T firstVertex) {
 		this.firstVertex = firstVertex;
 	}
 	
-	public void setSecondVertex(Vertex secondVertex) {
+	public void setSecondVertex(Vertex_T secondVertex) {
 		this.secondVertex = secondVertex;
 	}
 	
 
-	public void setValue(T value) {
+	public void setValue(Value_T value) {
 		this.value = value;
 	}
 	
--- Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Tree.java
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Tree.java
@@ -7,14 +7,14 @@ import java.util.List;
  * {@link Tree} représente un arbre/graphe non orienté (Théorie des graphes).
  * Plus précisément un graphe orienté dont les sommets sont
  * basés sur une classe implémentant {@link Vertex}
- * @param <V_T> Le type d'un sommet, sous contrainte qu'il étende {@link Vertex}
- * @param <E_T> Le type de valeur d'un arc
+ * @param <Vertex_T> Le type d'un sommet, sous contrainte qu'il étende {@link Vertex}
+ * @param <Edge_T> Le type de valeur d'un arc
  * @author jfourmond
  */
-public class Tree<V_T extends Vertex, E_T > {
+public class Tree<Vertex_T extends Vertex, Edge_T > {
 
-	private List<V_T> vertices;
-	private List<Edge<E_T>> edges;
+	private List<Vertex_T> vertices;
+	private List<Edge<Vertex_T, Edge_T>> edges;
 	
 	//	CONSTRUCTEURS
 	public Tree() {
@@ -22,28 +22,28 @@ public class Tree<V_T extends Vertex, E_T > {
 		edges = new ArrayList<>();
 	}
 	
-	public Tree(List<V_T> vertices) { this.vertices = vertices; }
+	public Tree(List<Vertex_T> vertices) { this.vertices = vertices; }
 	
-	public Tree(List<V_T> vertices, List<Edge<E_T>> edges) {
+	public Tree(List<Vertex_T> vertices, List<Edge<Vertex_T, Edge_T>> edges) {
 		this.vertices = vertices;
 		this.edges = edges;
 	}
 	
 	//	GETTERS
-	public List<V_T> getVertices() { return vertices; }
+	public List<Vertex_T> getVertices() { return vertices; }
 	
-	public List<Edge<E_T>> getEdges() { return edges; }
+	public List<Edge<Vertex_T, Edge_T>> getEdges() { return edges; }
 	
 	//	SETTERS
-	public void setVertices(List<V_T> vertices) { this.vertices = vertices; }
+	public void setVertices(List<Vertex_T> vertices) { this.vertices = vertices; }
 	
-	public void setEdges(List<Edge<E_T>> edges) { this.edges = edges; }
+	public void setEdges(List<Edge<Vertex_T, Edge_T>> edges) { this.edges = edges; }
 	
 	/**
 	 * Ajout d'un sommet dans l'arbre
 	 * @param vertex : le {@link Vertex} à ajouter
 	 */
-	public void addVertex(V_T vertex) {
+	public void addVertex(Vertex_T vertex) {
 		// TODO Vérifier que le sommet n'existe pas déjà
 		vertices.add(vertex);
 	}
@@ -52,7 +52,7 @@ public class Tree<V_T extends Vertex, E_T > {
 	 * Ajout d'un arc dans l'arbre
 	 * @param edge : le {@link Edge} à ajouter
 	 */
-	public void addEdges(Edge<E_T> edge) {
+	public void addEdges(Edge<Vertex_T, Edge_T> edge) {
 		// TODO vérifier que les sommets de l'arc existe
 		edges.add(edge);
 	}
@@ -60,11 +60,11 @@ public class Tree<V_T extends Vertex, E_T > {
 	@Override
 	public String toString() {
 		String ch = "Sommets : \n";
-		for(V_T vertex : vertices) {
+		for(Vertex_T vertex : vertices) {
 			ch += vertex.fullData();
 		}
 		ch += "Edges : \n";
-		for(Edge<E_T> edge : edges) {
+		for(Edge<Vertex_T, Edge_T> edge : edges) {
 			ch += "\t" + edge + "\n";
 		}
 		return ch;
