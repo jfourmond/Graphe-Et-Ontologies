65d738cce8829a547ad75202a7331685aed37c28
jfourmond <jerome@fourmond.fr>
2016-04-11 13:49:31 +0200

    Création d'une Exception pour Tree

---
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/TreeException.java
@@ -0,0 +1,11 @@
+package fr.fourmond.jerome.framework;
+
+public class TreeException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public TreeException() { super(); }
+
+	public TreeException(String s) { super(s); }
+	
+}
