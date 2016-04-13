9b1da9496c662f1c38a9d471b5134a9aeca981b5
jfourmond <jerome@fourmond.fr>
2016-04-13 16:09:56 +0200

    Nouveau build.xml (non généré par Eclipse)

--- Graphe-Et-Ontologies/src/Main.java
+++ Graphe-Et-Ontologies/src/Main.java
@@ -9,6 +9,7 @@ import fr.fourmond.jerome.view.TreeView;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Test...");
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception E) {
