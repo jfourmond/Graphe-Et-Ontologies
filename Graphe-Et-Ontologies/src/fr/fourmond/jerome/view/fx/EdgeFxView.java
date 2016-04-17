ee6a3a055bc1827fbe6acc3c4a1f547c40a7e2f3
Jérôme FOURMOND <jerome@fourmond.fr>
2016-04-17 15:34:24 +0200

    * Correction et Ajout affichage
    * EdgeFxView & VertexFxView : sous-classe de Group
    
    + label pour les sommets et les arcs
    
    * Commentaires

--- Graphe-Et-Ontologies/src/fr/fourmond/jerome/view/fx/EdgeFxView.java
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/view/fx/EdgeFxView.java
@@ -1,25 +1,49 @@
 package fr.fourmond.jerome.view.fx;
 
 import fr.fourmond.jerome.framework.Edge;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Line;
 
 /**
- * {@link EdgeFxView} est une {@link Line} représentant
+ * {@link EdgeFxView} est une {@link Group} représentant
  * un arc du graphe
  * @param <T> : type de la valeur de {@link Edge}
  * @author jfourmond
  */
-public class EdgeFxView<T> extends Line {
+public class EdgeFxView<T> extends Group {
 	private T value;
 	
+	private Line line;
+	private Label label;
+	
 	public EdgeFxView(T value, double startx, double starty, double endx, double endy) {
-		super(startx, starty, endx, endy);
+		super();
 		this.value = value;
+		
+		double middleX = (startx + endx) / 2;
+		double middleY = (starty + endy) / 2;
+		
+		line = new Line(startx, starty, endx, endy);
+		label = new Label("" + value);
+			label.setTranslateX(middleX);
+			label.setTranslateY(middleY);
+		
+		this.getChildren().add(line);
+		this.getChildren().add(label);
 	}
 
 	//	GETTERS
 	public T getValue() { return value; }
 	
+	public Line getLine() { return line; }
+
+	public Label getLabel() { return label; }
+	
 	//	SETTERS
 	public void setValue(T value) { this.value = value; }
+	
+	public void setLine(Line line) { this.line = line; }
+	
+	public void setLabel(Label label) { this.label = label; }
 }
--- Graphe-Et-Ontologies/src/fr/fourmond/jerome/view/fx/VertexFxView.java
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/view/fx/VertexFxView.java
@@ -2,33 +2,64 @@ package fr.fourmond.jerome.view.fx;
 
 
 import fr.fourmond.jerome.framework.Vertex;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 /**
- * {@link VertexFxView} est un cercle représentant
+ * {@link VertexFxView} est un {@link Group} représentant
  * un sommet du graphe
  * @param <T> : le type implémentant l'interface {@link Vertex}
  * @author jfourmond
  */
-public class VertexFxView<T extends Vertex> extends Circle {
+public class VertexFxView<T extends Vertex> extends Group {
 	private static int diameter = 25;
 	
+	private Circle circle;
+	private Label label;
 	private T vertex;
 	
 	public VertexFxView(T vertex, int x, int y) {
-		super(x, y, diameter, Color.TRANSPARENT);
-		super.setStroke(Color.BLACK);
-		
+		super();
 		this.vertex = vertex;
+		
+		circle = new Circle(x, y, diameter, Color.TRANSPARENT);
+		circle.setStroke(Color.BLACK);
+		
+		label = new Label(vertex.briefData());
+		label.setTranslateX(circle.getCenterX());
+		label.setTranslateY(circle.getCenterY());
+		
+		getChildren().add(circle);
+		getChildren().add(label);
 	}
 	
 	//	GETTERS
 	public T getVertex() { return vertex; }
 	
+	public Circle getCircle() { return circle; }
+	
 	public static int getDiameter() { return diameter; }
 	
 	//	SETTERS
 	public void setVertex(T vertex) { this.vertex = vertex; }
+	
+	public void setCircle(Circle circle) { this.circle = circle; }
+	
+	public double getCenterX() { return circle.getCenterX(); }
+	
+	public double getCenterY() { return circle.getCenterY(); }
+	
+	public void setCenterX(double value) {
+		circle.setCenterX(value);
+		label.setTranslateX(value);
+	}
+	
+	public void setCenterY(double value) {
+		circle.setCenterY(value);
+		label.setTranslateY(value);
+	}
+	
 	
 }
