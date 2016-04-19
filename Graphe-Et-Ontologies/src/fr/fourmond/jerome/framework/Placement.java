f70c197ccbaa6c90e1f6b2cc9851be2e63d47291
Jérôme FOURMOND <jerome@fourmond.fr>
2016-04-19 14:31:49 +0200

    Ajout de quelques positions

--- Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Placement.java
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Placement.java
@@ -6,30 +6,23 @@ import java.util.Random;
 /**
  * {@link Placement} est une classe reprenant (dans le concept) la classe {@link Random}.
  * Elle permet de créer un {@link Object} envoyant différentes positions à chaque appel de next().
+ * 24 {@link Point} peuvent être renvoyés "statiquement" avant d'être générés aléatoirement.
  * @author jfourmond
  */
 public class Placement {
 	public final static Point[] positions = {
 		// First Rectangle
-		new Point(100, 100),
-		new Point(400, 100),
-		new Point(400, 400),
-		new Point(100, 400),
+		new Point(100, 100), new Point(400, 100), new Point(400, 400), new Point(100, 400),
 		// Second Rectangle
-		new Point(250, 50),
-		new Point(450, 250),
-		new Point(250, 450),
-		new Point(50, 250),
+		new Point(250, 50), new Point(450, 250), new Point(250, 450), new Point(50, 250),
 		// Third Rectangle
-		new Point(175, 75),
-		new Point(425, 175),
-		new Point(325, 425),
-		new Point(75, 325),
+		new Point(175, 75), new Point(425, 175), new Point(325, 425), new Point(75, 325),
 		// Fourth Rectangle
-		new Point(75, 175),
-		new Point(325,  75),
-		new Point(425,  325),
-		new Point(175, 425)
+		new Point(75, 175), new Point(325, 75), new Point(425, 325), new Point(175, 425),
+		// Fifth Rectangle
+		new Point(175, 125), new Point(325, 125), new Point(325, 325), new Point(175, 325),
+		// ...
+		new Point(250, 100), new Point(400, 250), new Point(250, 400), new Point(100, 250)
 	};
 	
 	public Random rand;
