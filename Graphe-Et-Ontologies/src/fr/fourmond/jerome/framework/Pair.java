c16e571106177df467a28fd1febb8e400cc21ee6
Jérôme FOURMOND <jerome@fourmond.fr>
2016-04-22 18:18:00 +0200

    Déplacement de la classe Pair

--- Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Pair.java
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/framework/Pair.java
@@ -1,9 +1,17 @@
-package fr.fourmond.jerome.ontology;
+package fr.fourmond.jerome.framework;
 
+/**
+ * {@link Pair} est une classe générique permettant
+ * la gestion de pair d'objets
+ * @param <T_First> : type du premier objet
+ * @param <T_Second> : type du second objet
+ * @author jfourmond
+ */
 public class Pair<T_First, T_Second> {
 	private T_First first;
 	private T_Second second;
 	
+	//	CONSTRUCTEURS
 	public Pair() { }
 	
 	public Pair(T_First first, T_Second second) {
@@ -11,11 +19,16 @@ public class Pair<T_First, T_Second> {
 		this.second = second;
 	}
 
+	//	GETTERS
 	public T_First getFirst() { return first; }
 
 	public T_Second getSecond() { return second; }
 	
+	//	SETTERS
 	public void setFirst(T_First first) { this.first = first; }
 	
 	public void setSecond(T_Second second) { this.second = second; }
+	
+	@Override
+	public String toString() { return "( " + first + " , " + second + " )"; }
 }
