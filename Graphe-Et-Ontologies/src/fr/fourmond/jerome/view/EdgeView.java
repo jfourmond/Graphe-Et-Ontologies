5c09aab276980209cd691bf13e2ba45fe3528e4e
Jérôme FOURMOND <jerome@fourmond.fr>
2016-04-23 17:23:43 +0200

    Ajout EdgeView

---
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/view/EdgeView.java
@@ -0,0 +1,71 @@
+package fr.fourmond.jerome.view;
+
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
+public class EdgeView extends Group {
+private String relation;
+	
+	private Color color;
+	
+	private Line line;
+	private Label label;
+	
+	public EdgeView(String relation, double startx, double starty, double endx, double endy) {
+		super();
+		this.relation = relation;
+		
+		double middleX = (startx + endx) / 2;
+		double middleY = (starty + endy) / 2;
+		
+		color = Color.BLACK;
+		
+		line = new Line(startx, starty, endx, endy);
+		line.setStroke(color);
+		label = new Label("" + relation);
+			label.setTranslateX(middleX);
+			label.setTranslateY(middleY);
+		
+		this.getChildren().add(line);
+		this.getChildren().add(label);
+	}
+	
+	public EdgeView(String relation, double startx, double starty, double endx, double endy, Color color) {
+		super();
+		this.relation = relation;
+		
+		double middleX = (startx + endx) / 2;
+		double middleY = (starty + endy) / 2;
+		
+		this.color = color;
+		
+		line = new Line(startx, starty, endx, endy);
+		line.setStroke(color);
+		label = new Label("" + relation);
+			label.setTranslateX(middleX);
+			label.setTranslateY(middleY);
+		
+		this.getChildren().add(line);
+		this.getChildren().add(label);
+	}
+
+	//	GETTERS
+	public String getRelation() { return relation; }
+	
+	public Color getColor() { return color; }
+	
+	public Line getLine() { return line; }
+
+	public Label getLabel() { return label; }
+	
+	//	SETTERS
+	public void setRelation(String relation) { this.relation = relation; }
+	
+	public void setColor(Color color) { this.color = color; }
+	
+	public void setLine(Line line) { this.line = line; }
+	
+	public void setLabel(Label label) { this.label = label; }
+}
