45ff355061468de17f695ef3b1b941ab6e207bf0
Jérôme FOURMOND <jerome@fourmond.fr>
2016-04-23 17:41:54 +0200

    Ajout VertexList (pour la gestion des listes)

---
+++ Graphe-Et-Ontologies/src/fr/fourmond/jerome/view/VertexList.java
@@ -0,0 +1,12 @@
+package fr.fourmond.jerome.view;
+
+import javafx.scene.control.ListCell;
+
+public class VertexList extends ListCell<VertexView> {
+	@Override
+	protected void updateItem(VertexView item, boolean empty) {
+		super.updateItem(item, empty);
+		if(item != null)
+			setText(item.getVertex().toString());
+	}
+}
